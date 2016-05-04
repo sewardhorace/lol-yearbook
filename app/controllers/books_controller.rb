5c39291ad783f7e196309d12a5c93529dd94bd09
Max White <maxjwhite22@gmail.com>
2016-05-03 22:09:56 -0400

    refactor

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -28,49 +28,13 @@ class BooksController < ApplicationController
     summoner_id =  params[:summoner_id]
     book = Book.find_by(summoner_id: summoner_id)
     book.update_summoner(RiotApi.summoner_by_id(summoner_id))
-    #TODO ^this kind of sucks.. what if RiotApi call fails for some reason?
-    update_champions(book)
+    book.update_champions(RiotApi.champion_mastery(summoner_id))
+    #TODO ^needs error handling.. what if RiotApi call fails for some reason?
     render json: true
   end
 
   private
   def search_params
     params.require(:summoner_name)
-  end
-
-  #TODO refactor into class method
-  def create_champion(champion_id, book_id)
-    if champ = Champion.find_by(champion_id: champion_id, book_id: book_id) then
-      return champ
-    elsif champ = Champion.create(champion_id: champion_id, book_id: book_id) then
-      return champ
-    # elsif data = RiotApi.champion_by_id(champion_id) then
-    #   url = RiotApi.champion_image_url(data["image"]["full"])
-    #   champ = Champion.new(
-    #     champion_id: data["id"],
-    #     book_id: book_id,
-    #     name: data["name"],
-    #     title: data["title"],
-    #     img_url: url
-    #   )
-    #   return champ
-    else
-      return false
-    end
-  end
-
-  #TODO refactor into class method
-  def update_champions(book)
-    if champions = RiotApi.champion_mastery(book.summoner_id) then
-      champions.each do |mastery_data|
-        champ = create_champion(mastery_data["championId"], book.id)
-        champ.highest_grade = mastery_data["highestGrade"]
-        champ.mastery_points = mastery_data["championPoints"]
-        champ.mastery_level = mastery_data["championLevel"]
-        champ.save
-      end
-    else
-      return false
-    end
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -8,4 +8,26 @@ class Book < ActiveRecord::Base
     end
     return self
   end
+
+  def update_champions(champion_data)
+    #TODO activerecord rollback catch
+    champion_data.each do |mastery_data|
+      champion_id = mastery_data["championId"]
+      if champ = Champion.find_by(champion_id: champion_id, book_id: self.id) then
+        champ.update(
+          highest_grade: mastery_data["highestGrade"],
+          mastery_points: mastery_data["championPoints"],
+          mastery_level: mastery_data["championLevel"]
+        )
+      else
+        Champion.create(
+          book_id: self.id,
+          champion_id: champion_id,
+          highest_grade: mastery_data["highestGrade"],
+          mastery_points: mastery_data["championPoints"],
+          mastery_level: mastery_data["championLevel"]
+        )
+      end
+    end
+  end
 end
