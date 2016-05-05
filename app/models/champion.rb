38ef5d77198ec5f200d40e3a5adf288d05fad010
Max White <maxjwhite22@gmail.com>
2016-05-05 16:54:49 -0400

    champion show styles & data

--- app/models/champion.rb
+++ app/models/champion.rb
@@ -2,4 +2,23 @@ class Champion < ActiveRecord::Base
   belongs_to :book
   has_many :comments
   has_one :static_data, class_name: "StaticChampion", foreign_key: "champion_id", primary_key: "champion_id"
+
+  def mastery_title
+    return "Lvl #{self.mastery_level} Mastery"
+  end
+
+  def school_level
+    return case self.mastery_level
+    when 5
+      "Senior"
+    when 4
+      "Junior"
+    when 3
+      "Sophomore"
+    when 2
+      "Freshman"
+    else
+      "Incoming"
+    end
+  end
 end
