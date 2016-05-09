991bfe47d5ed8c8d2d52bc62eb3b1b443512c6cd
Max White <maxjwhite22@gmail.com>
2016-05-08 22:46:24 -0400

    limit update frequency

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,6 +10,9 @@ class Book < ActiveRecord::Base
   end
 
   def update_champions(champion_data)
+    # if !self.updateable? then
+    #   return false
+    # end
     #TODO activerecord rollback catch
     champion_data.each do |mastery_data|
       champion_id = mastery_data["championId"]
@@ -23,6 +26,22 @@ class Book < ActiveRecord::Base
         mastery_level: mastery_data["championLevel"],
         chest_earned: mastery_data["chestGranted"]
       )
+    end
+  end
+
+  def updateable
+    if champion = self.champions.take then
+      champion.updated_at < 1.day.ago
+    else
+      return true
+    end
+  end
+
+  def next_update_time
+    if champion = self.champions.take then
+      champion.updated_at.to_datetime + 1.day
+    else
+      return DateTime.now
     end
   end
 end
